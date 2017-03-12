fa4624db0f312585fbbc5c653467ce8cf0bec777
pietroglyph <declanfreemangleason@gmail.com>
2017-03-12 10:19:22 -0700

    Added a coustom toolbar with a check to settings.

--- app/src/main/java/com/spartronics4915/argos/SettingsActivity.java
+++ app/src/main/java/com/spartronics4915/argos/SettingsActivity.java
@@ -5,6 +5,8 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 
 public class SettingsActivity extends AppCompatActivity {
@@ -15,9 +17,13 @@ public class SettingsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_settings);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+    }
 
-        // If we go up we should display the EntryActivity
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    // Add our custom action bar
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_settings, menu);
+        return true;
     }
 
 }
